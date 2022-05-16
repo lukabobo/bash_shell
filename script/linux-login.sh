4af8be7575f01a7cf58f5ad255b5e22575358a1a
dungz1207 <51751623+lukabobo@users.noreply.github.com>
2022-05-16 16:44:59 +0700

    linux-login

---
+++ script/linux-login.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo ""
+echo "Welcome to vitechcheap.com"
+echo ""
+echo `date`
+echo ""
+echo "____   ____.__  __                .__           .__                          "
+echo "\   \ /   /|__|/  |_  ____   ____ |  |__   ____ |  |__   ____ _____  ______  "
+echo " \   Y   / |  \   __\/ __ \_/ ___\|  |  \_/ ___\|  |  \_/ __ \\__  \ \____ \ "
+echo "  \     /  |  ||  | \  ___/\  \___|   Y  \  \___|   Y  \  ___/ / __ \|  |_> >"
+echo "   \___/   |__||__|  \___  >\___  >___|  /\___  >___|  /\___  >____  /   __/ "
+echo "                         \/     \/     \/     \/     \/     \/     \/|__|    "
+echo ""
+echo " * Trang chu    : https://vitechcheap.com/"
+echo " * Facebook     : https://www.facebook.com/vitechcheap"
+echo " * Email ho tro : support@vitechcheap.Com"
+echo " * So dien thoai: 0963233720"
+echo ""
+echo "*----------------------------------------------------*"
+echo ""
